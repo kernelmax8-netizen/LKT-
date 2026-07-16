6f4f8c92840c75abd2da29d873569cb25eff326e
peddi-nikitha <peddinikitha66@gmail.com>
2026-07-16 12:05:38 +0530

    otp functionality done and dynamic

--- website/src/lib/firebase.js
+++ website/src/lib/firebase.js
@@ -12,6 +12,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  
 }
 
 export const app = initializeApp(firebaseConfig)
